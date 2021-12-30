93055c9f858ad410e5623d5b00991563b9a7c870
Cameron Trow <tumblegamer@gmail.com>
2021-12-30 14:00:04 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp beat-saver.desktop ~/.local/share/applications
-cp beat-saver.sh ~/.local/share/
+cp beat-saver.desktop ~/.local/share/applications -v
+cp beat-saver.sh ~/.local/share/ -v
 
 
 sed -i 's/beat-saver.sh/~\/.local\/share\/beat-saver.sh/' ~/.local/share/applications/beat-saver.desktop
